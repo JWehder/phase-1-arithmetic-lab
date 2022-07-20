2c2a578791a0aa8fe706662fab238a369f1ea473
Jake Wehder <jw@email.comjake>
2022-07-19 21:01:00 -0500

    commit tree 00b6ca18bb911590ccb7e3d67cdc3309983e9719

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(Number.isInteger(random));
+
+let num3 = 10;
+let num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 4, 20)
+max;
